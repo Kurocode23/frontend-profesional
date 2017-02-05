6018e3a5d89b8ca23f2000493f868d2cb4594fb2
Kurocode23 <alanaliaga23@gmail.com>
2017-02-05 17:52:14 -0400

    added marvel-game card jeduan

--- curso-de-javascript-y-jQuery/marvel-game-card/lib/app.js
+++ curso-de-javascript-y-jQuery/marvel-game-card/lib/app.js
@@ -10,10 +10,22 @@ class MarvelApi {
 
   findSeries (title) {
     var url = `${this.baseUrl}/series?title=${title}&apikey=${key}`
-    return Promise.resolve($.get(url))
-    .then((res) => {
-      return res.data.results[0]
-    })
+    if (localStorage[url]) {
+      var datos = localStorage[url]
+      datos = JSON.parse(datos)
+      console.log('Hola desde el cache')
+      return Promise.resolve(datos)
+    } 
+    else {
+      return Promise.resolve($.get(url))
+      .then((res) => {
+        var datos = res.data.results[0]
+        datos = JSON.stringify(datos)
+        localStorage[url] = datos
+        return Promise.resolve(datos)
+      })
+    }
+    
   }
 
   getResourceURI (resourceURI) {
@@ -21,10 +33,31 @@ class MarvelApi {
     // ¿Podrías crear un método interno al que llamen estos dos?
     let url = `${resourceURI}?apikey=${this.key}`
 
+    if (localStorage[url]) {
+      let datos = localStorage[url]
+      datos = JSON.parse(datos)
+      console.log('Hola desde el cache')
+      return Promise.resolve(datos)
+    }
+
     return Promise.resolve($.get(url))
     .then((res) => {
       let datos = res.data.results[0]
+      datos = JSON.stringify(datos)
+      localStorage[url] = datos
       return Promise.resolve(datos)
+    })
+  }
+
+  searchCharacter (name) {
+    let url = `${this.baseUrl}/characters?name=${name}&apikey${this.key}`
+    return new Promise (function (done) {
+      $.get(url).done(function (data) {
+        done(data)
+      })
+    })
+    .then(function () {
+      debugger
     })
   }
 }
@@ -58,18 +91,56 @@ api.findSeries('avengers')
   })
 })
 .then((results) => {
-  $('.Card').each((i, item) => {
-    let character = results[i]
-    let $this = $(item)
-    let $image = $this.find('.Card-image')
-    let $description = $this.find('.Card-description')
-    let $name = $this.find('.Card-name')
 
-    $image.attr('src', `${character.thumbnail.path}.${character.thumbnail.extension}`)
-    $name.text(character.name)
-    $description.text(character.description)
-  })
+  let characters = shuffle(results)
+
+  for (let i = 0; i<5; i++) {
+    let character = characters[i] 
+    let template = renderCharacter(character)
+    let $card = $(template)
+    $('.Battle-player').append($card)
+    $($card).on('click', function () {
+      let $this = $(this)
+      let attack = $this.find('.Card-attack')
+      console.log(attack.data('attack'))
+    })
+  }
+    
 })
 .catch((err) => {
   console.log(err)
 })
+
+$('.CharacterForm').on('submit', function (e) {
+  e.preventDefault()
+  let name = $(this).find('')
+})
+
+
+function renderCharacter (character) {
+
+  // Genera un numero de 500 a 1000
+  let attackPoints = Math.ceil(Math.random() * 500) + 500
+
+  let template = `
+    <div class="Card">
+      <h2 class="Card-name">${character.name}</h2><img src="${character.thumbnail.path}.${character.thumbnail.extension}" alt="Wolverine" class="Card-image">
+      <div class="Card-description">${character.description}</div>
+      <div class="Card-attack" data-attack="${attackPoints}">${attackPoints} puntos de ataque</div>
+    </div>
+  `
+
+  return template
+}
+
+function shuffle (arr) {
+  for (var i = 0; i < arr.length; i++) {
+    debugger
+    let tmp = arr[i]
+    let index = Math.floor(Math.random() * arr.length - 1)
+    arr[i] = arr[index]
+    arr[index] = tmp
+  }
+
+  return arr
+}
