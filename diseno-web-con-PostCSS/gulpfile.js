4764b2be2de84d850ea913d5bc4ab1f07ccbe454
Kurocode23 <alanaliaga23@gmail.com>
2017-02-06 17:39:12 -0400

    Unificando media-queries con Mqpacker

--- diseno-web-con-PostCSS/gulpfile.js
+++ diseno-web-con-PostCSS/gulpfile.js
@@ -4,6 +4,12 @@ var browserSync = require('browser-sync').create()
 // var autoprefixer = require('autoprefixer')
 var cssnested = require('postcss-nested')
 var cssnext = require('postcss-cssnext')
+var mixins = require('postcss-mixins')
+var atImport = require('postcss-import')
+var lost = require('lost')
+var csswring = require('csswring')
+var rucksack = require('rucksack-css')
+var mqpacker = require('css-mqpacker')
 
 // Servidor de desarrollo
 gulp.task('serve', function () {
@@ -17,14 +23,20 @@ gulp.task('serve', function () {
 // Tarea para procesar el CSS
 gulp.task('css', function () {
   var processors = [
+    atImport(),
+    mixins(),
     cssnested,
+    lost(),
+    rucksack(),
     cssnext({
       // dar soporte a navegadores con mas de 5% de uso, tambien a ie8
       browsers: ['> 5%', 'ie 8']
-    })
+    }),
+    mqpacker(),
+    csswring()
   ]
 
-  return gulp.src('./src/*.css')
+  return gulp.src('./src/invie.css')
     .pipe(postcss(processors))
     .pipe(gulp.dest('./dist/css'))
     .pipe(browserSync.stream())
