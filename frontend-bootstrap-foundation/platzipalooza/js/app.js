19cfdc2f53d4d5752af9bd4e4b22cc05832c6dc9
Kurocode23 <alanaliaga23@gmail.com>
2017-02-10 16:17:14 -0400

    Menu offcanvas con Foundation

--- frontend-bootstrap-foundation/platzipalooza/js/app.js
+++ frontend-bootstrap-foundation/platzipalooza/js/app.js
@@ -1,1 +1,11 @@
 $(document).foundation()
+
+$('[data-toggle-dia]').click(function (ev) {
+  var panel = $(this).data('toggleDia')
+  $('#lineup-tabs').foundation('selectTab', panel)
+})
+
+const $offCanvas = $('#offCanvas')
+$offCanvas.find('li').click(function (ev) {
+  $offCanvas.foundation('close')
+})
