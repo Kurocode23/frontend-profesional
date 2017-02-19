21d37042a780dbe3c7af21b857f0b5ec326f1e79
Kurocode23 <alanaliaga23@gmail.com>
2017-02-19 18:00:58 -0400

    ready to second part

---
+++ curso-definitivo-javascript/platzigram_desafios/desafio-4/src/userpage/index.js
@@ -0,0 +1,34 @@
+import page from 'page'
+import title from 'title'
+import empty from 'empty-element'
+
+import header from './../header'
+import template from './template'
+
+page('/:username',  header, loadUser, function (ctx, next) {
+  title(`Platzigram - ${ctx.user.username}`)
+  var main = document.getElementById('main-container')
+  empty(main).appendChild(template(ctx.user))
+})
+
+page('/:username/:id',  header, loadUser, function (ctx, next) {
+  title(`Platzigram - ${ctx.user.username}`)
+  var main = document.getElementById('main-container')
+  empty(main).appendChild(template(ctx.user))
+
+  $(`#modal${ctx.params.id}`).openModal({
+    complete: function () {
+      const path = `/${ctx.params.username}`
+      page(path)
+    }
+  })
+})
+
+async function loadUser (ctx, next) {
+  try {
+    ctx.user = await fetch('/api/user/${ctx.params.username}').then(res => res.json())
+    next()
+  } catch (err) {
+    console.log(err)
+  }
+}
